fc38cb011aa047371e33f23c5b334dae951829a4
Kwaku Dapaah <kwa2the4@gmail.com>
2018-09-24 12:15:58 -0400

    started making the customer table

--- backend/db/migrations/20180918225551_customer_table.js
+++ backend/db/migrations/20180918225551_customer_table.js
@@ -1,6 +1,9 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('customers', function(tbl) {
+      tbl.increments();
+      tbl.string('name');
+  })
 };
 
 exports.down = function(knex, Promise) {
